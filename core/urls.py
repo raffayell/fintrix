4d2d8649cd30c6425f5b58460b6c5f1387e78fb9
raffayell <rafayel.mar@gmail.com>
2025-07-01 20:44:56 +0400

    Added static file dirs

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from dashboard.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name="home")
-]
+] 
